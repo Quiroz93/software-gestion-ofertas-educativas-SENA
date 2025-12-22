098db817cb45853e557e6e106c01945d428ae2c1
Quiroz93 <jose.quirozquiroz93@gmail.com>
2025-12-22 16:31:36 -0500

    creación del modelo -mcr de la tabla Centro

--- database/migrations/2025_12_17_224112_create_offers_table.php
+++ database/migrations/2025_12_17_224112_create_offers_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
+            // Información principal de la oferta
+            $table->string('denominacion');          // Nombre o título de la oferta
+            $table->string('nivel_formacion');       // Nivel de formación
+            $table->string('modalidad');             // Modalidad (virtual, presencial, mixta)
+            $table->string('jornada');               // Jornada (mañana, tarde, noche)
+            $table->string('red_tecnologica');       // Red tecnológica
+            $table->string('anio_lectivo');          // Año lectivo
+            $table->date('fecha');                   // Fecha de publicación o inicio
+            $table->string('codigo_programa');       // Código del programa
+            $table->integer('cupos')->nullable();    // Número de cupos disponibles
+            $table->text('descripcion')->nullable(); // Descripción de la oferta
+            $table->boolean('estado')->default(true); // Estado (activo/inactivo)
+            $table->unsignedBigInteger('centro_id');
+            $table->foreign('centro_id')->references('id')->on('centros')
+                ->nullable()->cascadeOnDelete(); // Centro asociado a la oferta (clave foránea)
             $table->timestamps();
         });
     }
