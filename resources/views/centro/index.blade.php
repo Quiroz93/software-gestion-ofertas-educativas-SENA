a20640d630da2012156c066f7c44f1569d961749
DylanS <saavedra.dylan17@gmail.com>
2025-12-29 11:17:57 -0500

    se agrego una tabla en la vista Index de centros

--- resources/views/centro/index.blade.php
+++ resources/views/centro/index.blade.php
@@ -6,7 +6,33 @@ Centros
 <div class="container-fluid">
     <button class="btn btn-success">Agregar Centro</button>
     <button class="btn btn-primary">Volver</button>
-    
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Direccion</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <button class="btn btn-info">Editar</button>
+                    <button class="btn btn-danger">Eliminar</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 @endsection
 
