f76418dc0e63036867ebbefae271cd6b73faf6e5
DylanS <saavedra.dylan17@gmail.com>
2025-12-31 11:20:44 -0500

    Se añadio una etiqueta h1 con el fin de realizar el titulo de la vista competencia.index

---
+++ resources/views/competencias/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Competencias')
+
+@section('content')
+
+<section>
+    <h1 class="text-center font-weight-bold mb-3">gestion de competencias</h1>
+</section>
+
+@endsection
