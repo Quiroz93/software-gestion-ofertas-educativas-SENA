7cdf1c6e219dd61d52a6e594af018e95eee38d19
DylanS <saavedra.dylan17@gmail.com>
2025-12-31 11:33:39 -0500

    Se agregaron botones de agregar y volver en la vista competencias.index

--- resources/views/competencias/index.blade.php
+++ resources/views/competencias/index.blade.php
@@ -8,4 +8,11 @@
     <h1 class="text-center font-weight-bold mb-3">gestion de competencias</h1>
 </section>
 
+<section>
+    <div class="container">
+        <a href="#" class="btn btn-success">Agregar Competencia</a>
+        <a href="#" class="btn btn-primary">Volver</a>
+    </div>
+</section>
+
 @endsection
