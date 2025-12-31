2a415d489be881359fc641d5b0fe3dcacc3df4f4
DylanS <saavedra.dylan17@gmail.com>
2025-12-31 11:40:04 -0500

    Se comento el codigo de la vista competencia.index

--- resources/views/competencias/index.blade.php
+++ resources/views/competencias/index.blade.php
@@ -5,13 +5,15 @@
 @section('content')
 
 <section>
+    {{-- Esta es la seccion del titulo de la vista index --}}
     <h1 class="text-center font-weight-bold mb-3">gestion de competencias</h1>
 </section>
 
 <section>
     <div class="container">
-        <a href="#" class="btn btn-success">Agregar Competencia</a>
-        <a href="#" class="btn btn-primary">Volver</a>
+        {{-- seccion de botones de accion --}}
+        <a href="{{-- enlace al controller --}}" class="btn btn-success">Agregar Competencia</a>
+        <a href="{{-- enlace al controller --}}" class="btn btn-primary">Volver</a>
     </div>
 </section>
 
