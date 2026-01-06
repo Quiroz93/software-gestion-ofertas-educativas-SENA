df85a05309abffb8047fcae3331eda0e7f427aa4
Quiroz93 <jose.quirozquiroz93@gmail.com>
2026-01-06 10:05:44 -0500

    politicas de permisos actualizadas para gestion de centros

--- app/Policies/CentroPolicy.php
+++ app/Policies/CentroPolicy.php
@@ -4,64 +4,46 @@ namespace App\Policies;
 
 use App\Models\Centro;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class CentroPolicy
 {
     /**
-     * Determina si el usuario puede ver cualquier modelo.
+     * Ver listado de centros
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view.centros');
-
+        return $user->can('view_centros');
     }
 
     /**
-     * Determina si el usuario puede ver el modelo.
+     * Ver un centro específico
      */
     public function view(User $user, Centro $centro): bool
     {
-        return $user->can('view.centros');
+        return $user->can('view_centros');
     }
 
     /**
-     * Determina si el usuario puede crear modelos.
+     * Crear centros
      */
     public function create(User $user): bool
     {
-        return $user->can('create.centros');
+        return $user->can('create_centros');
     }
 
     /**
-     * Determina si el usuario puede actualizar el modelo.
+     * Actualizar centros
      */
     public function update(User $user, Centro $centro): bool
     {
-        return $user->can('update.centros');
+        return $user->can('update_centros');
     }
 
     /**
-     * Determina si el usuario puede eliminar el modelo.
+     * Eliminar centros
      */
     public function delete(User $user, Centro $centro): bool
     {
-        return false;
-    }
-
-    /**
-     * Determina si el usuario puede restaurar el modelo.
-     */
-    public function restore(User $user, Centro $centro): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determina si el usuario puede eliminar permanentemente el modelo.
-     */
-    public function forceDelete(User $user, Centro $centro): bool
-    {
-        return false;
+        return $user->can('delete_centros');
     }
 }
