9dab2f0e1d4b7de4f2675731603b9c9801adc028
Quiroz93 <jose.quirozquiroz93@gmail.com>
2026-01-07 19:13:08 -0500

    redireccionamiento del login hacia la vista de home directamente

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,6 +11,13 @@
                 </div>
 
                 <!-- Links de navegación -->
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
+
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('panel de control') }}
