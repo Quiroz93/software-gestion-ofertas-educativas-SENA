d415b6d59b1c2b48ebd0fce0bb5f0978e4e51d2e
Quiroz93 <jose.quirozquiroz93@gmail.com>
2026-01-08 16:15:21 -0500

    Eliminación de tailwind del proyecto para evitar conflictos con AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -34,13 +34,13 @@ Route::middleware(['auth', 'verified',])->prefix('admin')->group(function () {
 });
 
 
-
+//Rutas de gestión de roles
 Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
     Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
     Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
     Route::get('/roles/edit/{role}', [RoleController::class, 'edit'])->name('roles.edit');
-    Route::patch('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+    Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
     Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 });
 
