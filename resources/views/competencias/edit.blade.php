3174f63c0267cff8440123963eeee829550d5044
DylanS <saavedra.dylan17@gmail.com>
2026-01-15 10:40:52 -0500

    se corrigio una cosa en la vista competencias.edit y Se crearon las vistas index, create, edit de nivel de formacion

--- resources/views/competencias/edit.blade.php
+++ resources/views/competencias/edit.blade.php
@@ -11,7 +11,7 @@
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="{{ route('centro.index') }}">Competencias</a></li>
-                <li class="breadcrumb-item active">Crear</li>
+                <li class="breadcrumb-item active">Editar</li>
             </ol>
         </div>
     </div>
---
+++ resources/views/nivel_formacion/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title', 'Nivel de Formacion')
+
+@section('content')
+
+<section>
+    {{-- Esta es la seccion del titulo de la vista index --}}
+    <h1 class="text-center font-weight-bold mb-3">gestion Nivel de formación</h1>
+</section>
+
+<section>
+    <div class="container">
+        {{-- seccion de botones de accion --}}
+
+        <a href="{{-- enlace al controller --}}" class="btn btn-success">Agregar Nivel</a>
+
+        <a href="{{-- enlace al controller --}}" class="btn btn-secondary">Volver</a>
+    </div>
+</section>
+
+<section>
+    <div class="container mt-4">
+        {{-- seccion de tabla de datos --}}
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nombre </th>
+                    <th>Descripciones</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- Ejemplo de fila de datos --}}
+                <tr>
+                    <td>{{-- logica de nombre --}}</td>
+                    <td>{{-- logica de descripciones --}}</td>
+                    <td>
+                        {{-- configurar logica de acciones --}}
+                    </td>
+                </tr>
+                {{-- Agregar mas filas segun los datos disponibles --}}
+            </tbody>
+        </table>
+    </div>
+</section>
+
+@endsection
