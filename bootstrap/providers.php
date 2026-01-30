34f8ed4a02b41bac185f51e2f2784d377139a589
José <jose.quirozquiroz93@gmail.com>
2026-01-30 15:40:34 -0500

    Fix: agregar EncryptionServiceProvider para resolver binding 'encrypter'

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ return [
     App\Providers\AuthServiceProvider::class,
     Illuminate\Cache\CacheServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\Encryption\EncryptionServiceProvider::class,
     Illuminate\Filesystem\FilesystemServiceProvider::class,
     Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
     Illuminate\Foundation\Providers\FoundationServiceProvider::class,
