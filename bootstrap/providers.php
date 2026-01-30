f10ec19c242a7f1d57d5180868eb42885f6ec019
José <jose.quirozquiroz93@gmail.com>
2026-01-30 15:33:19 -0500

    Fix: registrar service providers faltantes para resolver bindings
    - Agregar FilesystemServiceProvider para resolver binding 'files'
    - Agregar CacheServiceProvider para resolver binding 'cache'
    - Agregar DatabaseServiceProvider para resolver binding 'db'
    - Agregar ConsoleSupportServiceProvider para habilitar comandos artisan
    - Agregar TranslationServiceProvider y ViewServiceProvider
    
    Esto corrige errores de BindingResolutionException al ejecutar comandos artisan

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,10 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
+    Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
 ];
